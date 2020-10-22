a5ba1abd783204c1a3d899b392bddeefede9d474
Bastien Paul <bastien.paul@coppernic.fr>
2020-10-22 18:28:06 +0200

    [FEATURE] AccessProtectionHelper is multi packages aware !

--- core/src/main/java/fr/coppernic/lib/utils/os/AccessProtectionHelper.kt
+++ core/src/main/java/fr/coppernic/lib/utils/os/AccessProtectionHelper.kt
@@ -2,12 +2,23 @@ package fr.coppernic.lib.utils.os
 
 import android.content.Context
 import android.os.Binder
-import android.util.Pair
 import fr.coppernic.lib.utils.log.LogDefines
 import fr.coppernic.lib.utils.log.LogDefines.LOG
+import timber.log.Timber
+import java.util.*
 
 @Suppress("MemberVisibilityCanBePrivate")
-class AccessProtectionHelper constructor(val context: Context, val whiteList: HashSet<Pair<String, String>>) {
+/**
+ * Help to know if an application is allowed to connect to this service
+ *
+ * White list is built as followed :
+ *
+ * Map < Caller certificate's SHA-256 -> Set of packages that are corresponding to this certificate hash >
+ *
+ * @param context Android Context
+ * @param whiteList List of applications allowed to connect to service
+ */
+class AccessProtectionHelper constructor(val context: Context, val whiteList: Map<String, Set<Regex>>) {
 
     /**
      * Checks if process that binds to this service (i.e. the package name corresponding to the
@@ -25,10 +36,10 @@ class AccessProtectionHelper constructor(val context: Context, val whiteList: Ha
      * @return true if process is allowed
      */
     fun isUidAllowed(uid: Int): Boolean {
-        return isPackageAllowed(getPackageForUid(uid))
+        return arePackagesAllowed(getPackageForUid(uid))
     }
 
-    fun getPackageForUid(uid: Int): String {
+    fun getPackageForUid(uid: Int): Set<String> {
         val callingPackages = context.packageManager.getPackagesForUid(uid)
                 ?: throw RuntimeException("Should not happen. No packages associated to caller UID!")
 
@@ -37,7 +48,7 @@ class AccessProtectionHelper constructor(val context: Context, val whiteList: Ha
         // callingPackages contains more than one entry when sharedUserId has been used
         // No plans to support sharedUserIds due to many bugs connected to them:
         // http://java-hamster.blogspot.de/2010/05/androids-shareduserid.html
-        return callingPackages[0]
+        return callingPackages.toSet()
     }
 
     /**
@@ -45,17 +56,39 @@ class AccessProtectionHelper constructor(val context: Context, val whiteList: Ha
      *
      * @return true if process is allowed
      */
-    fun isPackageAllowed(packageName: String): Boolean {
-        val remoteSignature = AppHelper.getAppSignaturesSHA256(context, packageName)
+    fun arePackagesAllowed(packageNames: Set<String>): Boolean {
+        // 1. Get a list of signature for all package names
+        return packageNames.map {
+            AppHelper.getAppSignaturesSHA256(context, it).toLowerCase(Locale.US)
+        }.toSet().any() { remoteSignature ->
+            // 2. For each signature, check access validity
+            isPackageAllowed(remoteSignature, packageNames)
+        }
+    }
+
+    fun isPackageAllowed(remoteSignature: String, packageNames: Set<String>): Boolean {
+        // 3. Checking access validity for each packageNames
+
         if (LogDefines.verbose) {
-            LOG.trace("Checking if package $packageName with signature $remoteSignature is allowed to access privileged extension")
+            LOG.trace("Checking access for signature $remoteSignature and packages $packageNames")
         }
 
-        whiteList.forEach {
-            if (packageName.matches(it.first.toRegex()) && remoteSignature.equals(it.second, true)) {
-                return true
+        val packagePatterns = whiteList[remoteSignature]
+        return if (packagePatterns == null) {
+            if (LogDefines.verbose) {
+                Timber.v("No signature found in whitelist")
+            }
+            false
+        } else {
+            packageNames.any { name ->
+                packagePatterns.any { pattern ->
+                    name.matches(pattern).also {
+                        if (LogDefines.verbose) {
+                            LOG.trace("does $name matches ${pattern.pattern} : $it")
+                        }
+                    }
+                }
             }
         }
-        return false
     }
 }
